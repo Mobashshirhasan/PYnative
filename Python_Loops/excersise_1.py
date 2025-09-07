40dc9e3d1fe13e38d90cb8f105547e0ad073e441
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-07 10:53:05 +0000

    while_loop

---
+++ Python_Loops/excersise_1.py
@@ -0,0 +1,9 @@
+# Exercise 1: Print first 10 natural numbers using while loop
+
+# Initialize counter
+n = 1
+
+# While loop to print first 10 natural numbers
+while n <= 10:
+    print(n)
+    n += 1  # Increment counter
