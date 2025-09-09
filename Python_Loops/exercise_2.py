09cef8dff76c41684a75ca59fe081b697fb28112
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-09 04:59:54 +0000

    updated

--- Python_Loops/exercise_2.py
+++ Python_Loops/exercise_2.py
@@ -34,9 +34,9 @@ i = 1
 n = 5
 
 while i <= n:
-    j = 1
+    j = 1 
     while j <= i:
-        print(j, end=' ')
+        print(j, end='  ')
         j += 1
     print()  # New line after each row
     i += 1
