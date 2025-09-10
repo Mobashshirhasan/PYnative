b5c8810f90bc9d245de389c04caa82a21944569a
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-10 06:56:36 +0000

    exercise_04

---
+++ exercise_4.py
@@ -0,0 +1,11 @@
+"""Write a Python code to accept a string from the user and display characters
+present at an even index number.For example, str = "PYnative". so your code 
+should display ‘P’, ‘n’, ‘t’, ‘v’."""
+
+user_input = input("Enter a String: ")
+print(f"Original String is : {user_input}")
+
+count = len(user_input)
+print("Printing only even index ")
+for i in range(0 , count - 1 , 2):
+    print("index [", i ,"]", user_input[i])
