bb941c3f6e9d28c92f21d8e5cc212ba121265dd5
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-12 10:14:26 +0000

    Exercise_8

---
+++ Python_Loops/exercise_8.py
@@ -0,0 +1,14 @@
+"""Write a code to return True if the list’s first and last numbers are the same. 
+If the numbers are different, return False."""
+
+
+numbers_x = [10, 20, 30, 40, 10]
+# output True
+
+numbers_y = [75, 65, 35, 75, 30]
+# Output False
+
+if numbers_x == numbers_y:
+    print("True")
+else:
+    print("False")
