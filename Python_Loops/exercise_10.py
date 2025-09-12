d5574464e67d35b65f2fdb28a71b6e29f946ed86
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-12 10:47:04 +0000

    Exercise_10

---
+++ Python_Loops/exercise_10.py
@@ -0,0 +1,10 @@
+"""Write a Python code to find how often the substring “Emma” appears in the given string.
+
+Given:
+str_x = "Emma is good developer. Emma is a writer
+
+"""
+
+str_x = "Emma is good developer. Emma is a writer"
+count = str_x.count("Emma")
+print("Emma appeared ", count, " times")
