6b0ded53e56985a0d0cd0538ec33d95d6e20d2d8
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-16 11:17:33 +0000

    updated

--- Python_Loops/exercise_3.py
+++ Python_Loops/exercise_3.py
@@ -17,5 +17,12 @@
 
 # Print the table from 1 to 10: 
 for i in range(1, 11):
-    result = 2 * i
-    print(f" 2 * {i} = {result}")
+    print(f"Table of {i}:")
+for j in range(1, 11):
+    # print(f" {i} * {j} = {i * j}")
+    print(f"{i} × {j} = {i * j}")
+
+# for i in range(1, 11):
+#     print(f"Table of {i}:")
+#     for j in range(1, 11):
+#         print(f"{i} × {j} = {i * j}")
