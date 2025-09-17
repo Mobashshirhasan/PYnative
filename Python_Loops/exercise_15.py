c55077b262eba5898a172a78ff27b1758c68f856
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-17 11:32:24 +0000

    updated

--- Python_Loops/exercise_15.py
+++ Python_Loops/exercise_15.py
@@ -5,5 +5,5 @@ for i in range(1, 21):
     print(f"Table of {i}:")
     for j in range(1, 11):
         result = i * j
-        print(f" {i} * {j} = {result}")
+        print(f" {j} * {i} = {result}")
     print()
