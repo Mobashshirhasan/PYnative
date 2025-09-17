980c1d8117304df72540bba11a4930c8eb0dd5b4
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-17 11:23:53 +0000

    Printing tables

---
+++ Python_Loops/exercise_15.py
@@ -0,0 +1,9 @@
+print("Multiplication Tables from 1 to 10:")
+print()
+
+for i in range(1, 21):
+    print(f"Table of {i}:")
+    for j in range(1, 11):
+        result = i * j
+        print(f" {i} * {j} = {result}")
+    print()
