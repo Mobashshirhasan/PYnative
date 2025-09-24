28d732ffdbfa701761a460ac63cdd48d21d36a39
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-24 12:01:42 +0000

    Fibonacci

---
+++ Python_Loops/Fibonacci.py
@@ -0,0 +1,13 @@
+"""Generate Fibonacci series up to 15 terms """
+
+
+num1 = 0
+num2 = 1
+
+print("Fibonacci series:")
+for num in range(15):
+    print(num1)
+    result = num1 + num2
+    num1 = num2
+    num2 = result
+    
