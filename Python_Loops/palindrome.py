d2e1ef0b8f1e6837c3c28a4055b70f0b9109db82
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-24 05:34:57 +0000

    Plaindrome

---
+++ Python_Loops/palindrome.py
@@ -0,0 +1,11 @@
+""" A palindrome number is a number that remains the same when its digits are 
+reversed. In simpler terms,it reads the same forwards and backward. 
+For example 121, 5005.
+"""
+
+p = str(input("Enter a number: "))
+r = p[::-1]
+if p == r:
+    print(f"{p} is a palindrome number")
+else:
+    print(f"{p} is not a palindrome number")
