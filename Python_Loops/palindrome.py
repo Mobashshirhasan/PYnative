2098464808e6c4fc61c96204a3b690c9bdc3f1cb
Mobashshir Hasan <mobashshir12@gmail.com>
2025-09-24 06:07:05 +0000

    updated

--- Python_Loops/palindrome.py
+++ Python_Loops/palindrome.py
@@ -3,7 +3,9 @@ reversed. In simpler terms,it reads the same forwards and backward.
 For example 121, 5005.
 """
 
-p = str(input("Enter a number: "))
+p = int(input("ENTER A NUMBER: "))
+
+p = str(p)
 r = p[::-1]
 if p == r:
     print(f"{p} is a palindrome number")
