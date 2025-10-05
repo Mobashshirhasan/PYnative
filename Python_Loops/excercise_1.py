693c165889ce46efeba051093de6dda060b1c6cd
Mobashshir Hasan <mobashshir12@gmail.com>
2025-10-05 12:07:56 +0000

    updated

--- Python_Loops/excercise_1.py
+++ Python_Loops/excercise_1.py
@@ -1,9 +1,20 @@
 # Exercise 1: Print first 10 natural numbers using while loop
 
 # Initialize counter
-n = 1
+# n = 1
 
-# While loop to print first 10 natural numbers
-while n <= 10:
-    print(n)
-    n += 1  # Increment counter
+# # While loop to print first 10 natural numbers
+# while n <= 10:
+#     print(n)
+#     n += 1  # Increment counter
+
+
+
+
+i = 1
+while i< 6:
+    print(i)
+    i = i + 1
+    
+    
+    
