0dc66f9c3aee59fe360db776899536319bf40ca4
mobashshirhasan <mobashshir12@gmail.com>
2025-10-07 22:56:45 +0530

    updated

--- Python_Loops/billing.py
+++ Python_Loops/billing.py
@@ -19,16 +19,16 @@ for i in list_of_items:
     total_amount = total_amount + i 
 print("Total Amount : ", total_amount)
 
-if total_amount >= 2000:
-    discounted_amount = (total_amount*10)/100
+if total_amount >= 10000:
+    discounted_amount = (total_amount*30)/100
     print("Discounted Amount: ", discounted_amount)
 
 elif total_amount >= 3000:
     discounted_amount = (total_amount*20)/100
     print("Discounted Amount: ", discounted_amount)
 
-elif total_amount >= 10000:
-    discounted_amount = (total_amount*30)/100
+elif total_amount >= 2000:
+    discounted_amount = (total_amount*10)/100
     print("Discounted Amount: ", discounted_amount)
     
 net_amount = total_amount - discounted_amount
