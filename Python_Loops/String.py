6d2c1021a38b56ecc266c67061793464430df459
mobashshirhasan <mobashshir12@gmail.com>
2025-10-07 22:53:33 +0530

    updated

--- Python_Loops/String.py
+++ Python_Loops/String.py
@@ -1,12 +1,20 @@
-import time
-import os  # To run Mac system commands
+# import time
+# import os  # To run Mac system commands
 
-countdown = int(input("Enter countdown time in seconds: "))
+# countdown = int(input("Enter countdown time in seconds: "))
 
-while countdown > 0:
-    print("Countdown:", countdown)
-    time.sleep(1)
-    countdown -= 1
+# while countdown > 0:
+#     print("Countdown:", countdown)
+#     time.sleep(1)
+#     countdown -= 1
 
-print("Time's up!")
-os.system("say 'Time is up!'")  # Mac will speak this out loud!
+# print("Time's up!")
+# os.system("say 'Time is up!'")  # Mac will speak this out loud!
+
+
+a = 0
+arr = [1000, 2000, 3000]
+
+for i in arr:
+    a = i + a
+print(a)
