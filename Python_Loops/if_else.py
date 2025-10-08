4985467fa9663928e1a939da6b25f003eb85f528
mobashshirhasan <mobashshir12@gmail.com>
2025-10-08 19:55:50 +0530

    if else

---
+++ Python_Loops/if_else.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age : "))
+
+if (age < 0):
+    print("Enter your number is Negative.")
+elif (age >= 18):
+    print("You are Eligible to vote.")
+
+else:
+    print("You are not Eligible to vote. Go watch POGO")
