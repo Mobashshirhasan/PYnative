47608bdff25a3457a32bb4959df712c5ca217af9
mobashshirhasan <mobashshir12@gmail.com>
2025-10-09 00:01:53 +0530

    updated

--- Python_Loops/if_else.py
+++ Python_Loops/if_else.py
@@ -1,9 +1,11 @@
-age = int(input("Enter your age : "))
+# age = int(input("Enter your age : "))
 
-if (age < 0):
-    print("Enter your number is Negative.")
-elif (age >= 18):
-    print("You are Eligible to vote.")
+# if (age < 0):
+#     print("Enter your number is Negative.")
+# elif (age >= 18):
+#     print("You are Eligible to vote.")
 
-else:
-    print("You are not Eligible to vote. Go watch POGO")
+# else:
+#     print("You are not Eligible to vote. Go watch POGO")
+
+
