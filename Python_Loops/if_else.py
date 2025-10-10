830441e38778dca72937b703b6f5ad9e0c37fbb6
mobashshirhasan <mobashshir12@gmail.com>
2025-10-10 15:46:11 +0530

    updated

--- Python_Loops/if_else.py
+++ Python_Loops/if_else.py
@@ -9,3 +9,17 @@
 #     print("You are not Eligible to vote. Go watch POGO")
 
 
+import time
+
+# start = time.strftime('%H')
+start = 3
+print(start)
+
+if int(start) >= 21 or int(start) <= 6 :
+    print(f"Good Night", start)
+elif int(start) >= 7 or int(start) <= 12:
+    print(f"Good Morning",start)
+elif int(start) >= 13 or int(start) <= 16:
+    print(f"Good Afternoon", start)
+else:
+    print("Good Evening")
