da34d2ce2584274a92e5dfa424976b638c4e6bea
mobashshirhasan <mobashshir12@gmail.com>
2025-10-10 20:14:35 +0530

    update

--- Python_Loops/Fibonacci.py
+++ Python_Loops/Fibonacci.py
@@ -1,13 +1,32 @@
 """Generate Fibonacci series up to 15 terms """
 
 
+# num1 = 0
+# num2 = 1
+
+# print("Fibonacci series:")
+# for num in range(10):
+#     print(num1)
+#     result = num1 + num2
+#     num1 = num2
+#     num2 = result
+    
+
+
+
 num1 = 0
 num2 = 1
+dig = 8
+print("Fabonnaci Series. ")
+print(num1)
+print(num2)
 
-print("Fibonacci series:")
-for num in range(10):
-    print(num1)
-    result = num1 + num2
+for num in range(2, dig):
+    # print(num1)
+
+    temp = num1 + num2
+    print(temp)
+
     num1 = num2
-    num2 = result
-    
+    num2 = temp
+# print(num2)
