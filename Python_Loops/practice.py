0da6d9d65394c9290ca0e54767e046c175ab91d6
mobashshirhasan <mobashshir12@gmail.com>
2025-10-12 07:11:53 +0530

    practice

---
+++ Python_Loops/practice.py
@@ -0,0 +1,6 @@
+# """ Concatinate two lists"""
+# a = [1,2,3,4,5]
+# b = [6,7,8,9,10]
+# a.extend(b)
+# # c = a + b
+# print(a)
--- Python_Loops/strings.py
+++ Python_Loops/strings.py
@@ -8,4 +8,35 @@
 
 
 
+""" Shift all the zeros to the right of the list"""
+# arr = [1, 2, 0, 3, 0, 4, 5]
 
+# l = len(arr)
+# print(l)
+
+# for num in arr:
+#     # print(f"element :", num)
+#     if num == 0:
+#         arr.remove(num)
+#         arr.append(num)
+# print(arr)
+
+
+
+
+
+
+""" Duplicate Array """
+arr = [1, 2, 2, 3, 40, 40, 58, 69, 69, 100, 102, 102]
+
+print([len(arr)])
+new_arr = []
+
+for num in range(len(arr)-1):
+    # print(arr[num])
+    if arr[num] != arr[num + 1]:
+        new_arr.append(arr[num])
+
+new_arr.append(arr[len(arr)-1])
+# print(new_arr)
+print(arr[len(arr)-1])
